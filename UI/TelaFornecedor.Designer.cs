049dd93cca7257b2849b8a2519e981e96280c578
curso.ads2 <curso.ads2@LAB6020>
2023-04-12 21:58:04 -0300

    Adicionar arquivos de projeto.

---
+++ UI/TelaFornecedor.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Camadas
+{
+    partial class TelaFornecedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtRazao = new System.Windows.Forms.TextBox();
+            this.lblRazao = new System.Windows.Forms.Label();
+            this.lblCnpj = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtCnpj = new System.Windows.Forms.MaskedTextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(20, 24);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(22, 16);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(114, 24);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtRazao
+            // 
+            this.txtRazao.Location = new System.Drawing.Point(126, 68);
+            this.txtRazao.Name = "txtRazao";
+            this.txtRazao.Size = new System.Drawing.Size(423, 20);
+            this.txtRazao.TabIndex = 3;
+            // 
+            // lblRazao
+            // 
+            this.lblRazao.AutoSize = true;
+            this.lblRazao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRazao.Location = new System.Drawing.Point(20, 68);
+            this.lblRazao.Name = "lblRazao";
+            this.lblRazao.Size = new System.Drawing.Size(100, 16);
+            this.lblRazao.TabIndex = 2;
+            this.lblRazao.Text = "Razão Social";
+            // 
+            // lblCnpj
+            // 
+            this.lblCnpj.AutoSize = true;
+            this.lblCnpj.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCnpj.Location = new System.Drawing.Point(20, 122);
+            this.lblCnpj.Name = "lblCnpj";
+            this.lblCnpj.Size = new System.Drawing.Size(46, 16);
+            this.lblCnpj.TabIndex = 4;
+            this.lblCnpj.Text = "CNPJ";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(297, 122);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(69, 16);
+            this.lblTelefone.TabIndex = 6;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // txtCnpj
+            // 
+            this.txtCnpj.Location = new System.Drawing.Point(114, 122);
+            this.txtCnpj.Name = "txtCnpj";
+            this.txtCnpj.Size = new System.Drawing.Size(152, 20);
+            this.txtCnpj.TabIndex = 8;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(20, 177);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(51, 16);
+            this.lblEmail.TabIndex = 9;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(385, 122);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(152, 20);
+            this.txtTelefone.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(114, 177);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(423, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(49, 223);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(104, 35);
+            this.btnSalvar.TabIndex = 12;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(262, 226);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(104, 35);
+            this.btnAtualizar.TabIndex = 13;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(433, 226);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(104, 35);
+            this.btnExcluir.TabIndex = 14;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(15, 288);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(711, 150);
+            this.dgvDados.TabIndex = 15;
+            // 
+            // TelaFornecedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtCnpj);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblCnpj);
+            this.Controls.Add(this.txtRazao);
+            this.Controls.Add(this.lblRazao);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Name = "TelaFornecedor";
+            this.Text = "TelaFornecedor";
+            this.Load += new System.EventHandler(this.TelaFornecedor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtRazao;
+        private System.Windows.Forms.Label lblRazao;
+        private System.Windows.Forms.Label lblCnpj;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.MaskedTextBox txtCnpj;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.MaskedTextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView dgvDados;
+    }
+}
---
+++ UI/TelaFornecedor.cs
@@ -0,0 +1,66 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Camadas
+{
+    public partial class TelaFornecedor : Form
+    {
+        public TelaFornecedor()
+        {
+            InitializeComponent();
+        }
+
+        Fornecedor forn;
+        private void CarregaTabela()
+        {
+            forn = new Fornecedor();
+            dgvDados.DataSource = forn.Pesquisar();
+        }
+        private void TelaFornecedor_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            forn = new Fornecedor();
+            forn.Cnpj = txtCnpj.Text;
+            forn.Email = txtEmail.Text;
+            forn.Telefone = txtTelefone.Text;
+            forn.Razao = txtRazao.Text;
+            MessageBox.Show(forn.Gravar() ? "Salvar com sucesso!" :
+                "Não foi possível salvar!");
+            CarregaTabela();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            forn = new Fornecedor();
+            forn.Id=int.Parse(txtId.Text);
+            MessageBox.Show(forn.Excluir() ? "Excluído com sucesso!":
+                "Não foi possível exlcuir!");
+                CarregaTabela();
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            forn = new Fornecedor();
+            forn.Id = int.Parse(txtCnpj.Text);
+            forn.Cnpj = txtCnpj.Text;
+            forn.Email = txtEmail.Text;
+            forn.Telefone = txtTelefone.Text;
+            forn.Razao = txtRazao.Text;
+            MessageBox.Show(forn.Atualizar() ? "Atualizado com sucesso!" :
+                "Não foi possível atualizar!");
+            CarregaTabela();
+        }
+    }
+}
